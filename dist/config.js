c07da540682671f7d02b1f7bf00a2a8579540567
Pratiksha <jmninfotech2021@gmail.com>
2026-05-16 11:03:40 +0530

    second

---
+++ dist/config.js
@@ -0,0 +1,7 @@
+window.__TOOLCRIBE_CONFIG__ = {
+  // Primary API endpoint used by the app.
+  PRIMARY_API_BASE_URL: 'http://103.182.196.254:2026/api/',
+
+  // SignalR hub used by issue/return screens.
+  RFID_HUB_URL: 'http://localhost:5149/rfidHub',
+};
